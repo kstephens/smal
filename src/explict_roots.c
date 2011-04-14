e4e287f9232f172a4d73f3606edb972dbeebab66
Kurt Stephens <ks.github@kurtstephens.com>
2011-04-14 14:27:05 -0500

    Added global explicit roots support.

--- src/explict_roots.c
+++ src/explict_roots.c
@@ -9,18 +9,46 @@
 
 #include <assert.h>
 #include <stdlib.h> /* malloc(), free() */
+#include <string.h> /* memcpy() */
 #include <stdio.h>
+
+static
+smal_roots *global_roots;
+
+int smal_root_add_global(smal_roots *roots)
+{
+  int result = -1;
+  smal_roots *r = 0;
+
+  if ( (r = malloc(sizeof(*r))) ) {
+    size_t size = sizeof(r->_bindings[0]) * roots->_bindings_n;
+    r->_bindings_n = roots->_bindings_n;
+    if ( (r->_bindings = malloc(size)) ) {
+      memcpy(r->_bindings, roots->_bindings, size);
+      r->_next = global_roots;
+      global_roots = r;
+      result = 0;
+    }
+  }
+
+  if ( result < 0 ) {
+    if ( r ) {
+      if ( r->_bindings )
+	free(r->_bindings);
+      free(r);
+    }
+  }
+
+  return result;
+}
 
 static
 void mark_roots(smal_roots *roots)
 {
-  if ( ! roots )
-    roots = smal_roots_current();
-  
   while ( roots ) {
     int i;
     /* fprintf(stderr, "  roots %p [%d]\n", roots, roots->_n); */
-    for ( i = 0; i < roots->_n; ++ i ) {
+    for ( i = 0; i < roots->_bindings_n; ++ i ) {
       void *ptr = * (void**) roots->_bindings[i];
       /* fprintf(stderr, "  mark %p => %p\n", roots->_bindings[i], ptr); */
       smal_mark_ptr(ptr);
@@ -49,6 +77,7 @@ void mark_thread(smal_thread *t, void *arg)
 
 void smal_roots_mark_chain()
 {
+  mark_roots(global_roots);
   smal_thread_each(mark_thread, 0);
 }
 
