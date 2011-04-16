3287b55fef11e48b7aa754a9244caf7c2aeef8b9
Kurt Stephens <ks.github@kurtstephens.com>
2011-04-16 02:52:58 -0500

    Test reference queue marking.

--- t/reference_test_1.c
+++ t/reference_test_1.c
@@ -43,32 +43,42 @@ int main()
   smal_roots_4(x, y, ref, ref_queue);
 
   ref_queue = smal_reference_queue_create();
+
   my_cons_type = smal_type_for(sizeof(my_cons), my_cons_mark, 0);
   x = smal_alloc(my_cons_type);
   y = smal_alloc(my_cons_type);
+
   ref = smal_reference_create_weak(y, ref_queue);
   assert(smal_reference_referred(ref) == (void*) y);
+
   x->car = (my_oop) 1;
   x->cdr = ref;
   y->car = (my_oop) 2;
   y->cdr = (my_oop) 0;
+
   smal_collect();
   assert(smal_reference_referred(ref) == (void*) y);
   assert(smal_reference_queue_take(ref_queue) == 0);
 
+  // Direct link to y = 0, ref link still exists.
+  // assert weak reference cleared.
   y = 0;
   smal_collect();
   assert(smal_reference_referred(ref) == 0);
 
+  // Direct link to ref lost
+  // Assert reference queue marks queued reference.
   ref_keep = ref;
   ref = 0;
   smal_collect();
   assert(smal_reference_queue_take(ref_queue) == ref_keep);
   ref_keep = 0;
 
+  // Assert reference queue is not empty.
   smal_collect();
   assert(smal_reference_queue_take(ref_queue) == 0);
 
+  // Assert lost reference is immediately swept and not queued.
   ref = smal_reference_create_weak(x, ref_queue);
   ref = 0;
   smal_collect();
